4adc2545115fb5313c0c986b7b80d98818d69505
Kiyeon Roh <rohkiyeon@naver.com>
2023-10-27 00:55:44 +0900

    2week Answer

---
+++ HUFS_Object_Programming/2week/answer/print_a.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static float sum(float n, float m){
+        return n+m;
+    }
+
+    public static float sub(float n, float m){
+        return n-m;
+    }
+
+    public static float mul(float n, float m){
+        return n*m;
+    }
+
+    public static float div(float n, float m){
+        return n/m;
+    }
+}
